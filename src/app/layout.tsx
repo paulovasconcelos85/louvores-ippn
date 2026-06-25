cd7df6626e2d561b5c8a2d618a6f332f10bc9f18
Paulo Vasconcelos <vasconcelospaulorp@gmail.com>
2026-06-25 16:24:03 -0400

    feat: add Vercel Analytics integration to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import LanguageSwitcher from '@/components/LanguageSwitcher'
 import { LocaleProvider } from '@/i18n/provider'
@@ -36,6 +37,7 @@ export default async function RootLayout({
           {children}
           <LanguageSwitcher />
         </LocaleProvider>
+        <Analytics />
       </body>
     </html>
   )
